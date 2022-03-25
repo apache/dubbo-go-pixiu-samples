e0113feb8ec5a554bfaced355043e587dbd24a64
Randy <ztelur@gmail.com>
2022-03-25 16:44:17 +0800

    update

--- go.mod
+++ go.mod
@@ -5,16 +5,17 @@ go 1.15
 require (
 	dubbo.apache.org/dubbo-go/v3 v3.0.1-0.20220107110037-4496cef73dba
 	github.com/apache/dubbo-go-hessian2 v1.11.0
+	github.com/apache/dubbo-go-pixiu v0.0.0-20220321132145-e68ff8dd6c80
+	github.com/dubbogo/gost v1.11.22
 	github.com/dubbogo/grpc-go v1.42.7
 	github.com/dubbogo/triple v1.1.7
 	github.com/gin-gonic/gin v1.7.4
 	github.com/golang/protobuf v1.5.2
-	github.com/prometheus/common v0.29.0 // indirect
-	github.com/shirou/gopsutil v3.21.3+incompatible // indirect
+	github.com/spf13/cobra v1.1.1
+	github.com/stretchr/objx v0.2.0 // indirect
 	github.com/stretchr/testify v1.7.0
-	github.com/tklauser/go-sysconf v0.3.5 // indirect
+	go.etcd.io/etcd/client/pkg/v3 v3.5.0 // indirect
 	google.golang.org/grpc v1.43.0
 	google.golang.org/protobuf v1.27.1
-	vimagination.zapto.org/memio v0.0.0-20200222190306-588ebc67b97d // indirect
 
 )
---
+++ start_integrate_test.sh
@@ -0,0 +1,40 @@
+#
+#  Licensed to the Apache Software Foundation (ASF) under one or more
+#  contributor license agreements.  See the NOTICE file distributed with
+#  this work for additional information regarding copyright ownership.
+#  The ASF licenses this file to You under the Apache License, Version 2.0
+#  (the "License"); you may not use this file except in compliance with
+#  the License.  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+array=("dubbogo/simple/body")
+array+=("dubbogo/simple/mix")
+array+=("dubbogo/simple/proxy")
+array+=("dubbogo/simple/query")
+array+=("dubbogo/simple/uri")
+array+=("dubbogo/simple/resolve")
+array+=("dubbogo/simple/registry")
+array+=("dubbogo/simple/triple")
+
+#
+##http
+array+=("http/grpc")
+array+=("http/simple")
+## grpc proxy
+array+=("grpc")
+
+for((i=0;i<${#array[*]};i++))
+do
+	sh ./integrate_test.sh ${array[i]}
+	result=$?
+	if [ $result -gt 0 ]; then
+    exit $result
+	fi
+done
