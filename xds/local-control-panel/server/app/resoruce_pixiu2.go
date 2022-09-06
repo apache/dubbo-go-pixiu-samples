afafc5a5f6fa9eb3f2f66d5f15a786902b836a7f
nanno <675502062@qq.com>
2022-09-06 09:22:38 -0700

    Fix snapshot

--- xds/local-control-panel/server/app/resoruce_pixiu2.go
+++ xds/local-control-panel/server/app/resoruce_pixiu2.go
@@ -45,7 +45,7 @@ func makeListeners2() *pixiupb.PixiuExtensionListeners {
 	}}
 }
 
-func GenerateSnapshotPixiu2() cache.Snapshot {
+func GenerateSnapshotPixiu2() *cache.Snapshot {
 	ldsResource2, _ := anypb.New(makeListeners2())
 	cdsResource, _ := anypb.New(makeClusters())
 	snap, _ := cache.NewSnapshot("2",
@@ -62,5 +62,5 @@ func GenerateSnapshotPixiu2() cache.Snapshot {
 			},
 		},
 	)
-	return *snap
+	return snap
 }
