a4ef6a1ce10f591647f5adcdcdb3f68db4fbd77a
maxing <1501901576@qq.com>
2022-09-17 00:24:15 +0800

    ospp: add traffic filter sample

---
+++ dubbogo/simple/traffic/server/v2/server.go
@@ -0,0 +1,39 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func main() {
+	routers := []string{"/user", "/user/pixiu", "/prefix", "/health"}
+
+	for _, router := range routers {
+		msg := router[strings.LastIndex(router, "/")+1:]
+		http.HandleFunc(router, func(w http.ResponseWriter, r *http.Request) {
+			_, _ = w.Write([]byte(fmt.Sprintf(`{"server": "v2","message":"%s","status":200}`, msg)))
+		})
+	}
+
+	log.Println("Starting sample server ...")
+	log.Fatal(http.ListenAndServe(":1316", nil))
+}
