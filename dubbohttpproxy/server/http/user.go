91d99008cd2afba4f393ae22c5452a7022353a77
twotwotwo <yjt20061029@gmail.com>
2026-05-16 18:23:14 +0800

    Clarify comments in UserDB implementation
    Updated comments for clarity regarding UserDB structure and methods.

--- dubbohttpproxy/server/http/user.go
+++ dubbohttpproxy/server/http/user.go
@@ -38,9 +38,9 @@ func init() {
 
 var cache *UserDB
 
-// UserDB cache user.
+// UserDB caches users by user ID.
 type UserDB struct {
-	// key is name, value is user obj
+	// key is user ID, value is user obj
 	cacheMap map[string]*User
 	lock     sync.Mutex
 }
@@ -54,12 +54,12 @@ func (db *UserDB) Add(u *User) bool {
 	return true
 }
 
-// Get returns the user.
-func (db *UserDB) Get(n string) (*User, bool) {
+// Get returns the user for the given user ID.
+func (db *UserDB) Get(id string) (*User, bool) {
 	db.lock.Lock()
 	defer db.lock.Unlock()
 
-	r, ok := db.cacheMap[n]
+	r, ok := db.cacheMap[id]
 	return r, ok
 }
 
